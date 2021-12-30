ef1c67e5209f3c2b623f0e38af54de60e9293f3e
arianna choza <unachoza@gmail.com>
2021-12-30 18:23:00 -0500

    react router dom version 5 works! added about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from "./Components/About.js"
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <header className="App-header">
-       
           Working React APP by Arianna Choza
       </header>
+        <Switch>
+        <Route
+            exact
+            path="/about"
+            render={(props) => <About {...props} />}
+          />
+        </Switch>
+        </BrowserRouter>
     </div>
   );
 }
