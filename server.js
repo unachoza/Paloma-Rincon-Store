e8e758e3217169a38ffeaf101f00d8deaa751e24
arianna choza <unachoza@gmail.com>
2021-12-30 23:55:29 -0500

    added price to api

--- server.js
+++ server.js
@@ -7,25 +7,31 @@ const app = express();
 
 const palomaURL = "https://palomarincon.bigcartel.com/";
 const products = [];
+const getRandomPrice = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+console.log("hello");
 app.get("/products", (req, res) => {
   axios
     .get(palomaURL)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-      $(".product-list-item", html).each(function () {
-        const productImage = $(this).text();
+      $(".product-list-item", html).each(function (i) {
         let img = $(this).find("img").attr("src");
         $(this)
           .find("div > div > div .product-list-item-name")
           .each(function () {
             let imageTitle = $(this).text();
             products.push({
+              id: i,
               title: imageTitle,
               imageURL: img,
-            });git 
+              price: `${getRandomPrice(70, 90)} €`,
+            });
           });
       });
+
       res.json(products);
     })
     .catch((err) => {
